c318ea6c0d628ddd56150a81c0cab556146d01b9
ordon <ordon1313@gmail.com>
2015-12-26 18:52:30 +0100

    exec onhook script

--- atomas/hook.go
+++ atomas/hook.go
@@ -2,11 +2,13 @@ package atomas
 import (
 	"net/http"
 	"fmt"
+	"os/exec"
 )
 
 func CreateHookHandler(hooks *Hooks) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		hooks.Hooks = append(hooks.Hooks, r.RemoteAddr)
+		exec.Command("bash", "onhook").Start()
 		fmt.Fprint(w, ToJsonString(hooks))
 	}
 }
