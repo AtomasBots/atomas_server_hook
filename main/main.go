a9cff182c8712657674e697c7f4cb91e28ad9cdc
ordon <ordon1313@gmail.com>
2015-12-27 17:30:42 +0100

    version handler

--- main/main.go
+++ main/main.go
@@ -6,12 +6,14 @@ import (
 	"github.com/AtomasBots/atomas_server_hook/atomas"
 )
 
+var version string
 
 func main() {
 	hooks := atomas.Hooks{[]string{}}
 	http.HandleFunc("/hook", atomas.CreateHookHandler(&hooks))
 	http.HandleFunc("/hook_list", atomas.CreateHookListHandler(&hooks))
 	http.HandleFunc("/health", atomas.CreateHealthService())
+	http.HandleFunc("/version", atomas.CreateVersionHandler(version))
 	port := os.Args[1]
 	log.Fatal(http.ListenAndServe(port, nil))
 }
