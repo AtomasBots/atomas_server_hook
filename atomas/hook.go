e1653941e3fb367b12cbe437bb899abf901d4cfa
ordon <ordon1313@gmail.com>
2016-01-02 17:31:12 +0100

    Setup ip restriction #1

--- atomas/hook.go
+++ atomas/hook.go
@@ -3,12 +3,53 @@ import (
 	"net/http"
 	"fmt"
 	"os/exec"
+	"net"
 )
 
 func CreateHookHandler(hooks *Hooks) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
-		hooks.Hooks = append(hooks.Hooks, r.RemoteAddr)
-		exec.Command("bash", "onhook").Start()
-		fmt.Fprint(w, ToJsonString(hooks))
+		remoteIP := r.RemoteAddr
+		if (isOnWhiteList(remoteIP)) {
+			hooks.Hooks = append(hooks.Hooks, remoteIP)
+			exec.Command("bash", "onhook").Start()
+			fmt.Fprint(w, ToJsonString(hooks))
+		}else {
+			http.Error(w, "Frobidden", http.StatusForbidden)
+		}
 	}
+}
+
+func isOnWhiteList(remoteIP string) bool {
+	whiteList := []net.IPNet{
+		parseNet("192.30.252.0/22"),
+		parseNet("192.168.0.0/24"),
+		parseNet("127.0.0.1/8"),
+	}
+	return contains(whiteList, ParseIP(remoteIP))
+}
+
+func ParseIP(s string) net.IP {
+	ip, _, err := net.SplitHostPort(s)
+	if err == nil {
+		return net.ParseIP(ip)
+	} else {
+		return net.ParseIP(s)
+	}
+}
+
+func contains(list []net.IPNet, ip net.IP) bool {
+	for _, net := range list {
+		if (net.Contains(ip)) {
+			return true
+		}
+	}
+	return false
+}
+
+func parseNet(s string) net.IPNet {
+	_, n, err := net.ParseCIDR(s)
+	if err != nil {
+		panic(err)
+	}
+	return *n
 }
