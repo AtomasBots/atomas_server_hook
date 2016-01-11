1ce6ded3a11ce88052b511247678c728a8fa6790
ordon <ordon1313@gmail.com>
2016-01-11 22:47:27 +0100

    move rebooting to new repository

--- main/main.go
+++ main/main.go
@@ -23,28 +23,5 @@ func main() {
 	http.HandleFunc("/version", atomas.CreateVersionHandler(version))
 	port := os.Args[1]
 	go doOnHook()
-	go rebootWhenNoInternet()
 	log.Fatal(http.ListenAndServe(port, nil))
-}
-
-func rebootWhenNoInternet() {
-	time.Sleep(300)
-	for {
-		if (isWifiOk()) {
-			time.Sleep(time.Second)
-		} else {
-			time.Sleep(time.Second * 10)
-			if (!isWifiOk()) {
-				ioutil.WriteFile(time.Now().String(), []byte(time.Now().String()), 0644)
-				exec.Command("sudo", "reboot").Start()
-			}
-		}
-	}
-}
-
-func isWifiOk() bool {
-	command := exec.Command("ping", "-c", "1", "192.168.0.1")
-	command.Start()
-	err := command.Wait()
-	return err == nil
 }
